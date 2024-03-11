43f89ff3b5a56c2309071ad0b493a03ae7d4b07f
YurZig <ziganshin@bk.ru>
2024-03-11 16:47:52 +0300

    Form comumns

--- resources/views/admin/includes/_modal_columns.blade.php
+++ resources/views/admin/includes/_modal_columns.blade.php
@@ -1,13 +1,13 @@
-<div class="modal fade" id="columns-form" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+<div class="modal fade" id="columns-form" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Колонки</h5>
+                <h5 class="modal-title" id="modalLabel">Колонки</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form method="POST" action="{{ $action }}">
-                @csrf
-                <div class="modal-body">
+            <div class="modal-body">
+                <form method="POST" id="columns" action="{{ $action }}">
+                    @csrf
                     <ul class="column-list">
                         @foreach($fields as $field)
                         <li class="column-item">
@@ -15,19 +15,19 @@
                                 <input class="form-check-input"
                                        type="checkbox"
                                        name="fields[]"
-                                       {{ in_array($field['dbName'], $columns) ? ' checked' : ''}}
+                                       @checked(in_array($field['dbName'], $columns))
                                        value="{{ $field['dbName'] }}">
                                 {{ $field['name'] }}
                             </label>
                         </li>
                         @endforeach
                     </ul>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
-                    <button type="submit" class="btn btn-primary">Сохранить</button>
-                </div>
-            </form>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Закрыть</button>
+                <button type="submit" class="btn btn-primary" form="columns">Сохранить</button>
+            </div>
         </div>
     </div>
 </div>
